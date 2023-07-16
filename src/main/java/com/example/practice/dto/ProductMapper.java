3e80985a824be2ac96cde06d11336f389aea886b
IrinaCol28 <irina.col2626@gmail.com>
2023-07-16 13:03:34 +0300

    Лишние импорты

--- src/main/java/com/example/practice/dto/ProductMapper.java
+++ src/main/java/com/example/practice/dto/ProductMapper.java
@@ -7,7 +7,6 @@ import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface ProductMapper {
